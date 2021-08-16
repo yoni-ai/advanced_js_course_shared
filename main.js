212e2e879dea2e9c10bd12569a2a58f5f2d5ba57
yoni-ai <62334252+yoni-ai@users.noreply.github.com>
2021-08-16 13:08:53 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
 $(function(){
-
     $( "#github_content" ).hide();
     $( "#bootstrap_content" ).hide();
     $( "#node_content" ).hide();
@@ -13,18 +12,15 @@ $(function(){
         $( "#node_content" ).hide( "slow");
         $( "#home_content" ).show( "slow");
       },
-
       mouseenter: function(){
-        $(this).animate({width: "100%"}, 500);
+        $(this).animate({width: "100%"}, 300);
       },
-
       mouseleave: function(){
         $(this).animate({width: "50%"}, 500);
       }
-      
     }); 
   }
-
+  
     $("#github_enter").on({
       click: function(){
         $( "#home_content" ).hide("slow");
@@ -38,7 +34,6 @@ $(function(){
         $("#github4_content").hide("fast");
       }
     });
-
     $("#bootstrap_enter").on({
       click: function(){
         $( "#home_content" ).hide( "slow");
@@ -51,7 +46,6 @@ $(function(){
         $("#bootstrap3_content").hide("slow");
       }
     });
-
     $("#node_enter").on({
       click: function(){
         $("#github_content").hide("slow");
@@ -59,16 +53,16 @@ $(function(){
         $("#bootstrap_content").hide("slow");
         $("#node_content").show("slow");
         home_button();
+        $("#node1_content").hide("slow");
+        $("#node0_content").hide("slow");
         $("#node2_content").hide("slow");
         $("#node3_content").hide("slow");
         $("#node4_content").hide("slow");
         $("#node5_content").hide("slow");
         $("#node6_content").hide("slow");
-        $("#node1_content").show("slow");
- 
+        $("#nodei_content").show("slow");
       }
     });
-
     $("#github_1").on({
       click: function(){
         $("#github2_content").hide("fast");
@@ -77,7 +71,6 @@ $(function(){
         $("#github1_content").show("slow");
       }
     });
-
     $("#github_2").on({
       click: function(){
         $("#github1_content").hide("slow");
@@ -86,7 +79,6 @@ $(function(){
         $("#github2_content").show("slow");
       }
     });
-
     $("#github_3").on({
       click: function(){
         $("#github1_content").hide("slow");
@@ -95,7 +87,6 @@ $(function(){
         $("#github3_content").show("slow");
       }
     });
-
     $("#github_4").on({
       click: function(){
         $("#github1_content").hide("slow");
@@ -104,7 +95,6 @@ $(function(){
         $("#github4_content").show("slow");
       }
     });
-
     $("#bootstrap_1").on({
       click: function(){
         $("#bootstrap2_content").hide("slow");
@@ -112,7 +102,6 @@ $(function(){
         $("#bootstrap1_content").show("slow");
       }
     });
-
     $("#bootstrap_2").on({
       click: function(){
         $("#bootstrap1_content").hide("slow");
@@ -120,7 +109,6 @@ $(function(){
         $("#bootstrap2_content").show("slow");
       }
     });
-
     $("#bootstrap_3").on({
       click: function(){
         $("#bootstrap1_content").hide("slow");
@@ -128,9 +116,34 @@ $(function(){
         $("#bootstrap3_content").show("slow");
       }
     });
-
+    $("#node_intro").on({
+      click: function(){
+        $("#node0_content").hide("slow");
+        $("#node1_content").hide("fast");
+        $("#node2_content").hide("fast");
+        $("#node3_content").hide("fast");
+        $("#node4_content").hide("fast");
+        $("#node5_content").hide("fast");
+        $("#node6_content").hide("fast");
+        $("#nodei_content").show("slow");
+      }
+    });
+    $("#node_0").on({
+      click: function(){
+        $("#nodei_content").hide("slow");
+        $("#node1_content").hide("fast");
+        $("#node2_content").hide("fast");
+        $("#node3_content").hide("fast");
+        $("#node4_content").hide("fast");
+        $("#node5_content").hide("fast");
+        $("#node6_content").hide("fast");
+        $("#node0_content").show("slow");
+      }
+    });
     $("#node_1").on({
       click: function(){
+        $("#nodei_content").hide("slow");
+        $("#node0_content").hide("fast");
         $("#node2_content").hide("fast");
         $("#node3_content").hide("fast");
         $("#node4_content").hide("fast");
@@ -139,9 +152,10 @@ $(function(){
         $("#node1_content").show("slow");
       }
     });
-
     $("#node_2").on({
       click: function(){
+        $("#nodei_content").hide("slow");
+        $("#node0_content").hide("fast");
         $("#node1_content").hide("fast");
         $("#node3_content").hide("fast");
         $("#node4_content").hide("fast");
@@ -150,9 +164,10 @@ $(function(){
         $("#node2_content").show("slow");
       }
     });
-
     $("#node_3").on({
       click: function(){
+        $("#nodei_content").hide("slow");
+        $("#node0_content").hide("fast");
         $("#node2_content").hide("fast");
         $("#node1_content").hide("fast");
         $("#node4_content").hide("fast");
@@ -161,9 +176,10 @@ $(function(){
         $("#node3_content").show("slow");
       }
     });
-
     $("#node_4").on({
       click: function(){
+        $("#nodei_content").hide("slow");
+        $("#node0_content").hide("fast");
         $("#node2_content").hide("fast");
         $("#node3_content").hide("fast");
         $("#node1_content").hide("fast");
@@ -172,9 +188,10 @@ $(function(){
         $("#node4_content").show("slow");
       }
     });
-
     $("#node_5").on({
       click: function(){
+        $("#nodei_content").hide("slow");
+        $("#node0_content").hide("fast");
         $("#node2_content").hide("fast");
         $("#node3_content").hide("fast");
         $("#node1_content").hide("fast");
@@ -183,9 +200,10 @@ $(function(){
         $("#node5_content").show("slow");
       }
     });
-
     $("#node_6").on({
       click: function(){
+        $("#nodei_content").hide("slow");
+        $("#node0_content").hide("fast");
         $("#node2_content").hide("fast");
         $("#node3_content").hide("fast");
         $("#node1_content").hide("fast");
@@ -194,67 +212,7 @@ $(function(){
         $("#node6_content").show("slow");
       }
     });
-
-
-
-
-      // $('.lesson_choice').on({
-      //   click: function(){
-      //     // $(this).addClass('active');
-      //   }
-      // });
-
-      // $('.tablinks').click({
-
-      // });
-
-
-
-    // $("#github_enter").on({
-    //   click: function(){
-    //     $("#home_content").hide();
-    //     $("github_content").show();
-    //   }
-    // });
-
-    // $("#node_enter").on({
-    //   click: function(){
-    //     $(this).animate({
-    //       left: '20%',
-    //       height: '70px',
-    //       width: '150px',
-    //       fontSize: '1.2em',
-    //       paddingTop: '7%'
-    //     });
-    //   },
-    //     mouseenter: function () {
-    //       $(this).css("background-color", "rgb(255, 0, 0)");
-    //   },
-    //     mouseleave: function () {
-    //       $(this).css("background-color", "rgb(128, 0, 79)");
-    // }
-    // });
-
-    // $("p").on({
-    //   mouseenter: function(){
-    //     $(this).css("background-color", "rgb(218, 0, 0)");
-    //   },
-    //   mouseleave: function(){
-    //     $(this).css("background-color", "rgb(128, 0, 0)");
-    //   }
-    // }); 
-    // $("#github_class").on({
-    //   hover: function(){
-    //     $(this).innerHTML("background-color", "rgb(218, 0, 0)");
-    //   },
-    //   mouseleave: function(){
-    //     $(this).css("background-color", "rgb(128, 0, 0)");
-    //   }
-    // }); 
-
   }); 
-
-   
   $( window ).on( "load", function() {
     console.log( "window loaded" );
 });
